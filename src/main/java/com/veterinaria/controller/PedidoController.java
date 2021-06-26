7372d01902768e806415ca233987c430a7ed6798
Lulio <lulio1981@gmail.com>
2021-06-26 14:49:54 -0500

    "Integrando pedido probar"

--- src/main/java/com/veterinaria/controller/PedidoController.java
+++ src/main/java/com/veterinaria/controller/PedidoController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,46 +30,39 @@ import com.veterinaria.entity.Pedido;
 import com.veterinaria.entity.SeleccionProducto;
 import com.veterinaria.entity.SeleccionServicio;
 import com.veterinaria.service.IPedidoService;
-import com.veterinaria.service.IProductoService;
-import com.veterinaria.service.IUsuarioService;
 
+import javassist.NotFoundException;
 
-@CrossOrigin(origins = { "http://localhost:4200","https://patazas-62d1c.web.app","https://patazasvet.web.app" })
+@CrossOrigin(origins = { "http://localhost:4200", "https://patazas-62d1c.web.app", "https://patazasvet.web.app" })
 @RestController
 @RequestMapping("/api")
 public class PedidoController {
-	
-	@Autowired
-	private IUsuarioService usuarioService;
-	
-	@Autowired
-	private IProductoService productoService;
-	
+
 	@Autowired
 	private IPedidoService pedidoService;
-	
-	//aca se almacenan los productos seleccionados
+
+
+	// aca se almacenan los productos seleccionados
 	private List<SeleccionProducto> seleccionadosP = new ArrayList<SeleccionProducto>();
 
 	@RequestMapping("/verPedido")
 	public String verPedido() {
 		return "pedido";
 	}
-	
+
 	@RequestMapping("/agregarSeleccionProducto")
 	@ResponseBody
-	public List<SeleccionProducto> agregarProducto(SeleccionProducto obj){
+	public List<SeleccionProducto> agregarProducto(SeleccionProducto obj) {
 		seleccionadosP.add(obj);
 		return seleccionadosP;
 	}
-	
+
 	@RequestMapping("/listaSeleccionProducto")
 	@ResponseBody
-	public List<SeleccionProducto> listaProductos(){
+	public List<SeleccionProducto> listaProductos() {
 		return seleccionadosP;
 	}
-	
-	
+
 	@RequestMapping("/eliminaSeleccionProducto")
 	@ResponseBody
 	public List<SeleccionProducto> eliminarProducto(int idProducto) {
@@ -80,40 +74,32 @@ public class PedidoController {
 		}
 		return seleccionadosP;
 	}
-	
-	
-	///Servicio
-	
+
+	/// Servicio
+
 	private List<SeleccionServicio> seleccionaS = new ArrayList<SeleccionServicio>();
-	
+
 	/*
-	@RequestMapping("/verPedidoProducto")
-	public String verPedidoProducto() {
-		return "pedido";
-	}
-	
-	@RequestMapping("/verPedidoServicio")
-	public String verPedidoservicio() {
-		return "pedido";
-	}
-	*/
-	
-	
+	 * @RequestMapping("/verPedidoProducto") public String verPedidoProducto() {
+	 * return "pedido"; }
+	 * 
+	 * @RequestMapping("/verPedidoServicio") public String verPedidoservicio() {
+	 * return "pedido"; }
+	 */
+
 	@RequestMapping("/agregarSeleccionServicio")
 	@ResponseBody
-	public List<SeleccionServicio> agregarServicio(SeleccionServicio obj){
+	public List<SeleccionServicio> agregarServicio(SeleccionServicio obj) {
 		seleccionaS.add(obj);
 		return seleccionaS;
 	}
-	
-	
+
 	@RequestMapping("/listaSeleccionServicio")
 	@ResponseBody
-	public List<SeleccionServicio> lista(){
+	public List<SeleccionServicio> lista() {
 		return seleccionaS;
 	}
-	
-	
+
 	@RequestMapping("/eliminaSeleccionServicio")
 	@ResponseBody
 	public List<SeleccionServicio> eliminarServicio(int idServicio) {
@@ -125,43 +111,101 @@ public class PedidoController {
 		}
 		return seleccionaS;
 	}
-	
-	
-	
-	@Secured({ "ROLE_VENDEDOR", "ROLE_ADMIN", "ROLE_CLIENTE" })
-	//@GetMapping("/usuarios/{id}")
+
+	@Secured({ "ROLE_VETERINARIO", "ROLE_ADMIN", "ROLE_CLIENTE", "ROLE_RECEPCIONISTA", "ROLE_VENDEDOR" })
+	// @GetMapping("/usuarios/{id}")
 	@GetMapping("/pedidos/{id}")
-	//@GetMapping("/facturas/{id}")
+	// @GetMapping("/facturas/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<?> listById(@PathVariable int id) {
-		
+
 		Optional<Pedido> pedido = null;
 		Map<String, Object> response = new HashMap<>();
-		
-		pedido =  pedidoService.findById(id);
-		
-		response.put("PEDIDO",pedido);
-		
+
+		pedido = pedidoService.findById(id);
+
+		response.put("PEDIDO", pedido);
+
 		return ResponseEntity.ok(pedido);
 	}
-	
-	
-	@Secured({ "ROLE_VETERINARIO", "ROLE_ADMIN", "ROLE_CLIENTE" })
-	//@GetMapping("/usuarios/{id}")
+
+	@Secured({ "ROLE_VETERINARIO", "ROLE_ADMIN", "ROLE_CLIENTE", "ROLE_RECEPCIONISTA", "ROLE_VENDEDOR" })
+	@GetMapping("/pedidos/dni/{dni}")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<?> listByDni(@PathVariable String dni) {
+
+		return ResponseEntity.ok(pedidoService.buscarPorDniUsuario(dni));
+	}
+
+	@Secured({ "ROLE_VENDEDOR", "ROLE_ADMIN", "ROLE_CLIENTE", "ROLE_RECEPCIONISTA" })
+	@GetMapping("/pedidos/{dni}/{estado}")
+	public ResponseEntity<List<Pedido>> listAllCitasDniAndEstado(@PathVariable String dni,
+			@PathVariable String estado) {
+		return ResponseEntity.ok(pedidoService.findPedidoPorDniAndEstado(dni, estado));
+	}
+
+	@Secured({ "ROLE_VETERINARIO", "ROLE_ADMIN", "ROLE_CLIENTE", "ROLE_RECEPCIONISTA", "ROLE_VENDEDOR" })
+	// @GetMapping("/usuarios/{id}")
+	@GetMapping("/pedidos/usuario/{id}")
+	// @GetMapping("/facturas/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<?> listPedidosDeUsuario(@PathVariable int id) {
+
+		List<Pedido> pedido = null;
+		Map<String, Object> response = new HashMap<>();
+
+		pedido = pedidoService.findByUsuario(id);
+
+		response.put("pedido", pedido);
+
+		return ResponseEntity.ok(pedido);
+	}
+
+	@Secured({ "ROLE_VETERINARIO", "ROLE_ADMIN", "ROLE_CLIENTE", "ROLE_RECEPCIONISTA", "ROLE_VENDEDOR" })
+	@PutMapping("/pedidos/estado/{id}")
+	public ResponseEntity<Pedido> update(@RequestBody Pedido obj, @PathVariable int id) throws NotFoundException {
+		// obj.setFecha_reg(new Date())
+		Pedido pedidoActual = pedidoService.findById(id)
+				.orElseThrow(() -> new NotFoundException("Mascota not found for this id :: " + id));
+
+		/*
+		 * Employee employee = employeeRepository.findById(employeeId) .orElseThrow(()
+		 * -> new ResourceNotFoundException("Employee not found for this id :: " +
+		 * employeeId));
+		 */
+
+		pedidoActual.setEstado(obj.getEstado());
+
+		// String subject, String to, String text
+
+		/*
+		 * String body = "<h2>Nro : B-CT-000000"+obj.getIdpedido()+"</h2><br>"+
+		 * "<h4>Servicio Adquirido : "+ obj +"</h4><br>"+ "<h4> Costo : S/." + obj +
+		 * "</h4><br>"+ "";
+		 * 
+		 * emailService.sendEmailMessagePagoCita("Cita - Pago realizado",obj.getUsuario(
+		 * ).getCorreo(),body);
+		 */
+
+		final Pedido updatedPedio = pedidoService.estadoPedido(pedidoActual);
+		return ResponseEntity.ok(updatedPedio);
+	}
+
+	@Secured({ "ROLE_VENDEDOR", "ROLE_ADMIN", "ROLE_CLIENTE" })
+	// @GetMapping("/usuarios/{id}")
 	@PostMapping("/pedidos")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Pedido registrarPedido(@RequestBody Pedido ped,  BindingResult result){
-		
-			
-		//Map<String, Object> response = new HashMap<>();
-		
+	public Pedido registrarPedido(@RequestBody Pedido ped, BindingResult result) {
+
+		// Map<String, Object> response = new HashMap<>();
+
 		List<DetallePedidoProducto> detalleProducto = new ArrayList<DetallePedidoProducto>();
-		
+
 		for (DetallePedidoProducto x : ped.getDetallesProducto()) {
-			DetallePedidoProductoPK pk = new DetallePedidoProductoPK();		
-			
+			DetallePedidoProductoPK pk = new DetallePedidoProductoPK();
+
 			pk.setIdproducto(x.getProducto().getIdproducto());
-			//pk.setIdpedido(2);
+			// pk.setIdpedido(2);
 
 			DetallePedidoProducto detail = new DetallePedidoProducto();
 			detail.setCantidad(x.getCantidad());
@@ -170,15 +214,16 @@ public class PedidoController {
 
 			detalleProducto.add(detail);
 		}
-		
+
 		List<DetallePedidoServicio> detalleServicio = new ArrayList<DetallePedidoServicio>();
-		
+
 		for (DetallePedidoServicio x : ped.getDetallePedidoServicio()) {
-			DetallePedidoServicioPK pk = new DetallePedidoServicioPK();		
-			
-			//System.out.println("fechaaa "+ped.getDetallePedidoServicio().get(1).getFechaAtencion());
+			DetallePedidoServicioPK pk = new DetallePedidoServicioPK();
+
+			// System.out.println("fechaaa
+			// "+ped.getDetallePedidoServicio().get(1).getFechaAtencion());
 			pk.setIdServicio(x.getServicio().getIdservicio());
-			//pk.setIdpedido(2);
+			// pk.setIdpedido(2);
 			DetallePedidoServicio detail = new DetallePedidoServicio();
 			detail.setFecha_atencion(x.getFecha_atencion());
 			detail.setCantidad(x.getCantidad());
@@ -186,38 +231,28 @@ public class PedidoController {
 			detail.setDetallePedidoServicioPK(pk);
 			detalleServicio.add(detail);
 		}
-		
-		
+
 		/*
-		 * Boleta objBoleta = new Boleta();
-		objBoleta.setCliente(objCliente);
-		objBoleta.setUsuario(objUsuario);
-		objBoleta.setDetallesBoleta(detalles);
-		 * */
-		
+		 * Boleta objBoleta = new Boleta(); objBoleta.setCliente(objCliente);
+		 * objBoleta.setUsuario(objUsuario); objBoleta.setDetallesBoleta(detalles);
+		 */
+
 		Pedido pedido = new Pedido();
-		
+
 		pedido.setUsuario(ped.getUsuario());
-		//if(detalleProducto != null) {			
-			pedido.setDetallesProducto(detalleProducto);
-		//}else if() {
-			pedido.setDetallePedidoServicio(detalleServicio);
-		//}
-		
-		//pedidoService.insertaPedidoProducto(pedido);
-			//pedidoService.insertaPedidoServicio(pedido);
-			pedidoService.insertaPedido(pedido);
-			
-		
+		// if(detalleProducto != null) {
+		pedido.setDetallesProducto(detalleProducto);
+		// }else if() {
+		pedido.setDetallePedidoServicio(detalleServicio);
+		// }
+		pedido.setEstado("PAGADO");
+
+		// pedidoService.insertaPedidoProducto(pedido);
+		// pedidoService.insertaPedidoServicio(pedido);
+		pedidoService.insertaPedido(pedido);
+
 		return pedido;
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
--- src/main/java/com/veterinaria/service/ServicePedidoImpl.java
+++ src/main/java/com/veterinaria/service/ServicePedidoImpl.java
@@ -1,5 +1,6 @@
 package com.veterinaria.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
@@ -16,19 +17,20 @@ import com.veterinaria.entity.DetallePedidoServicio;
 import com.veterinaria.entity.Estado;
 import com.veterinaria.entity.Pedido;
 import com.veterinaria.entity.Tracking;
+import com.veterinaria.entity.Usuario;
 
 @Service
 public class ServicePedidoImpl implements IPedidoService {
-	
+
 	@Autowired
 	private IPedidoRepositoryDao pedidoRepository;
-	
+
 	@Autowired
 	private IDetallePedidoProductoDao detalleProductoRepository;
-	
+
 	@Autowired
 	private IDetallePedidoServicioDao detalleServicioDao;
-	
+
 	@Autowired
 	private ITrackingDao trackingDao;
 
@@ -36,39 +38,67 @@ public class ServicePedidoImpl implements IPedidoService {
 	@Transactional
 	public Pedido insertaPedido(Pedido obj) {
 		Pedido cabecera = pedidoRepository.save(obj);
-		
+
 		Estado est = new Estado();
-		
+
 		est.setIdestado(1);
-		
+
 		Tracking tracking = new Tracking();
 		tracking.setPedido(cabecera);
 		tracking.setEstado(est);
-		
+
 		trackingDao.save(tracking);
-		
+
 		for (DetallePedidoProducto d : cabecera.getDetallesProducto()) {
-			
+
 			d.getDetallePedidoProductoPK().setIdpedido(cabecera.getIdpedido());
-			//detalleProductoRepository.actualizaStock(d.getCantidad(), d.getProducto().getIdproducto());
+			// detalleProductoRepository.actualizaStock(d.getCantidad(),
+			// d.getProducto().getIdproducto());
 			detalleProductoRepository.save(d);
 		}
 		for (DetallePedidoServicio d : cabecera.getDetallePedidoServicio()) {
 			d.getDetallePedidoServicioPK().setIdPedido(cabecera.getIdpedido());
-			//detalleServicioDao.actualizaStock(d.getCantidad(), d.getProductoHasBoletaPK().getIdProducto());
+			// detalleServicioDao.actualizaStock(d.getCantidad(),
+			// d.getProductoHasBoletaPK().getIdProducto());
 			detalleServicioDao.save(d);
 		}
-		return cabecera ;
+		return cabecera;
 	}
-
-	
-
 
 	@Override
 	@Transactional
 	public Optional<Pedido> findById(int id) {
 		// TODO Auto-generated method stub
 		return pedidoRepository.findById(id);
+	}
+
+	@Override
+	public List<Pedido> findByUsuario(int id_usuario) {
+		// TODO Auto-generated method stub
+
+		Usuario usuario = new Usuario();
+		usuario.setIdusuario(id_usuario);
+		;
+
+		return pedidoRepository.findByUsuario(usuario);
+	}
+
+	@Override
+	public List<Pedido> buscarPorDniUsuario(String dni_user) {
+		// TODO Auto-generated method stub
+		return pedidoRepository.buscarPorDniUsuario(dni_user);
+	}
+
+	@Override
+	public List<Pedido> findPedidoPorDniAndEstado(String dni_user, String estado_user) {
+		// TODO Auto-generated method stub
+		return pedidoRepository.findPedidoPorDniAndEstado(dni_user, estado_user);
+	}
+
+	@Override
+	public Pedido estadoPedido(Pedido obj) {
+		// TODO Auto-generated method stub
+		return pedidoRepository.save(obj);
 	}
 
 }
